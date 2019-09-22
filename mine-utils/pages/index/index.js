8fc86925fa23c4951e1083afa0ce9c004711c547
wuwenwei <1650248655@qq.com>
2019-09-22 23:58:13 +0800

    feat(all): 项目整体架构搭建

--- mine-utils/pages/index/index.js
+++ mine-utils/pages/index/index.js
@@ -4,16 +4,9 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -49,6 +42,11 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+  toHome: function() {
+    wx.switchTab({
+      url: '../home/home',
     })
   }
 })
